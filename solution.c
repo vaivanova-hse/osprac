4e7be242504d4473aaee80167ac7f2bdac5a0a8a
vaivanova-hse <vaivanova_3@edu.hse.ru>
2021-01-25 13:32:20 +0300

    Lab 1 done.

---
+++ solution.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+        printf("ID user started the program: %d\n", getuid());
+        printf("ID user's group: %d\n", getgid());
+        return 0;
+}
