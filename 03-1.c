7ada330e2dd40e95120d5f69731cc32db1c5ceac
vaivanova-hse <vaivanova_3@edu.hse.ru>
2021-02-01 14:30:26 +0300

    Lab 3 done. Fix.

--- 03-1.c
+++ 03-1.c
@@ -10,11 +10,13 @@ int main()
   if (pid == -1){
     return -1;
   }
-  if (pid == 0){
+  else if (pid == 0){
     ppid = getppid();
+    pid = getpid();
     printf("Child-process, pid = %d, ppid = %d\n", (int)pid, (int)ppid);
   }
   else {
+    pid = getpid();
     ppid = getppid();
     printf("Parent-process, pid = %d, ppid = %d\n", (int)pid, (int)ppid);
   }
