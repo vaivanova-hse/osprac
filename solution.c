72645b62953c6a3df16351db4fe6ca3ac0e1290e
vaivanova-hse <63722133+vaivanova-hse@users.noreply.github.com>
2021-05-24 17:51:46 +0300

    Delete solution.c

--- solution.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main(){
-        printf("ID user started the program: %d\n", getuid());
-        printf("ID user's group: %d\n", getgid());
-        return 0;
-}
